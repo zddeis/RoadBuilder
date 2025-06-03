178e24acee6208c9c01ab97e324beb45b59d4cf7
zddeis <david.fcg07@gmail.com>
2025-06-03 09:23:26 +0100

    Version 0.6

--- variables.js
+++ variables.js
@@ -1,9 +1,10 @@
 const hyperlinks = {
     "discord" : "https://discord.gg/yFBbjpbkRw",
-    "devforum" : "https://devforum.roblox.com/t/plugin-roadbuilder-v040-build-roads-like-never-before/3620710",
+    "devforum": "https://devforum.roblox.com/t/plugin-roadbuilder-v050-introducing-crosswalks-auto-render/3650389",
     "store" : "https://create.roblox.com/store/asset/14674744762/",
 };
 
 const roadmapSheet = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQyCLna5PZiXSGNINkAHjl-bQOQkTY9pRUB0-SimqFXkqSFHq_qa4GkJ3vZHAoBjERrx3k2fCJ_AiEn/pub?gid=0&single=true&output=csv"
 
-const version = 0.5;
+const version = 0.6;
+const roadMapVersion = "0.6.1";
